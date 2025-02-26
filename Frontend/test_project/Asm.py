c63ad2d0c922f5f977472ba63882446b17f898eb
jason-van-de-berckt-stdent-ap <jason.vandeberckt@student.ap.be>
2025-02-26 10:40:08 +0100

    Changes Main file

--- Frontend/test_project/Asm.py
+++ Frontend/test_project/Asm.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, jsonify
 import dns.resolver
 import sqlite3
 import csv
-import json
 from datetime import datetime
 import requests
 import socket
