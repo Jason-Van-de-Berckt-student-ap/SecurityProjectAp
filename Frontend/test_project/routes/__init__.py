fdedbc2f59cd369959c0d4fecbab095a196610de
jason-van-de-berckt-stdent-ap <jason.vandeberckt@student.ap.be>
2025-03-12 16:29:10 +0100

    init patch

--- Frontend/test_project/routes/__init__.py
+++ Frontend/test_project/routes/__init__.py
@@ -7,11 +7,11 @@ from flask import Blueprint
 # Import all blueprints
 from routes.single_scan import single_scan_bp
 from routes.batch_scan import batch_scan_bp
-from Frontend.test_project.routes.tech_detection_depreciated import tech_detection_bp
+# from Frontend.test_project.routes.tech_detection_depreciated import tech_detection_bp
 
 # List of all blueprints for easy registration
 all_blueprints = [
     single_scan_bp,
     batch_scan_bp,
-    tech_detection_bp
+    # tech_detection_bp
 ]
